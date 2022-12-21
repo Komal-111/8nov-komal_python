34d06d31da5ab694957e13779eba1848a42f3753
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2022-12-21 16:59:28 +0530

    Add files via upload

---
+++ revrese string.py
@@ -0,0 +1,12 @@
+#revrese string
+def reverse_string(str):
+    if len(str)% 4 == 0   :
+        return ''.join(reversed(str))
+        return str1
+print(reverse_string('hi komal'))
+print(reverse_string('komal'))
+
+
+
+
+
