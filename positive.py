d11df9d89ed3cd6326fe272340421aa2661bbf5e
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2022-12-30 14:43:07 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,8 @@
+#programmme positive negative or zero
+num=int(input("Enter a number:"))
+if num>=0:
+    print("This  number is positive")
+else :
+    print(" This  number is nagative")
+if num==0:
+    print("this number is zero")
