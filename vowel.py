fceebe693db99badcd858b8dea17634f41baa901
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2022-12-30 14:47:23 +0530

    Add files via upload

--- vowel.py
+++ vowel.py
@@ -1,8 +1,11 @@
 #GIVEN CHARACTER IS VOWEL OR NOT
-ch = input("Enter a character: ")
 
-if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
- or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-    print(ch, "is a Vowel")
+ch=input("enter character:")
+
+if ch==('a','e','i,','o','u'):
+    print("the character is vowel.")
 else:
-    print(ch, "is a Consonant")
+    print("the character is not vowel.")
+
+    
+
