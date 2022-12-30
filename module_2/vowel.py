4491993d7b4bdad5017e22611c693e42c5ed5533
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2022-12-30 14:45:36 +0530

    Add files via upload

---
+++ module_2/vowel.py
@@ -0,0 +1,11 @@
+#GIVEN CHARACTER IS VOWEL OR NOT
+
+ch=input("enter character:")
+
+if ch==('a','e','i,','o','u'):
+    print("the character is vowel.")
+else:
+    print("the character is not vowel.")
+
+    
+
