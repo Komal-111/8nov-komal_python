9dd06e03c821451728e1bae71a121bf06d4a7981
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2022-12-30 14:47:00 +0530

    Add files via upload

---
+++ module_2/swaap2.py
@@ -0,0 +1,10 @@
+# TWO NUMBER SWAP USING THIRD VARIABLE
+print("Before swap values")
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+
+temp=a
+a=b
+b=temp
+print("after swap a:",a)
+print("after swap b:",b)
