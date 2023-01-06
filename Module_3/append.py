ac14c8f4f5f2641c1dbfd22bfddd1ced6daa4ae7
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-06 15:26:46 +0530

    Delete append.py

--- Module_3/append.py
+++
@@ -1,6 +0,0 @@
-#Write a Python program to append text to a file and display the text.
-f1=open('k1.txt',"a")
-id = input("enter your id:")
-stname= input("enter your name ")
-city= input ("enter your city name")
-f1.write(f"id:{id}\nstname:{stname}\ncity:{city}\n")
