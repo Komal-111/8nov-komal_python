409046db344360c7edbe7851862516ad761e0f6b
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-06 15:29:17 +0530

    Delete write into list.py

--- Module_3/write into list.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program to write a list to a file. 
-city = ['rajkot','broda','jamnagar','surat']
-with open('2.txt',"w") as fp:
-    for  items in city:
-    
-        fp.write("%s\n" % items)
-    print("done")
-list = city
-print(list)
