50b90eb24a317ef3e9cbc95f290e0acc2ffcac5a
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-06 15:27:42 +0530

    Delete first.py

--- Module_3/first.py
+++
@@ -1,8 +0,0 @@
-#Write a Python program to read an entire text file.
-f1= open("k1.txt", "r+")
-print(f1.readlines())
-f1.write(" hi students")
-f1.write(" hi python")
-f1.write(" hi komal")
-f1.write(" hi reema")
-f1.close()
