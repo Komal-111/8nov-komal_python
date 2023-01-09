de747cdc87a613d2e0e0d9c9ef633ccba5215a4b
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-09 15:32:02 +0530

    Add files via upload

---
+++ module_2/middle str.py
@@ -0,0 +1,6 @@
+def insert_sting_middle(str, komal):
+	return str[:2] + komal + str[2:]
+
+print(insert_sting_middle('[[]]', 'Python'))
+print(insert_sting_middle('{{}}', 'PHP'))
+print(insert_sting_middle('<<>>', 'HTML'))
