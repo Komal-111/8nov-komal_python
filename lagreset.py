bdba1714952bab6acc93d7e41991a74025fe23b0
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-09 13:59:12 +0530

    Delete lagreset.py

--- lagreset.py
+++
@@ -1,7 +0,0 @@
-#Write a Python program to find the second smallest number in a list. 
-list1= ['45','55','75','80','90']
-largest =len (list1)
-def find_len(list1):
-    length=len(list1)
-    list1.sort()
-    print("second largest number is",list1 [length -2])
