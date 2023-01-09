4da460dafb9653d23c0195b8ba568adf8ba1c1a6
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-09 15:28:12 +0530

    Add files via upload

---
+++ Module_3/random/parallaom.py
@@ -0,0 +1,6 @@
+#Write a Python program to calculate the area of a parallelogram
+
+b = float(input("WEnter Base of parallelogram"))
+h = float(input("Enter height of Parallelogramn"))
+area = b*h
+print("Area of parallelogram",area)
