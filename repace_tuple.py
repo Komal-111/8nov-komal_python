2e2fbe04a516d1c0bf7f26facee3b661c5e575bf
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-09 13:56:47 +0530

    Delete repace_tuple.py

--- repace_tuple.py
+++
@@ -1,8 +0,0 @@
-#Write a Python program to replace last value of tuples in a list
-
-compart = ('keyboard','mouse','monitor','cpu','harddisk')
-print(compart)
-a = list(compart)
-a[4] = 'joystick'
-compart=tuple(a)
-print(a)
