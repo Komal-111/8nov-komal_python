8181622a579296086b9a33a257ec9e9c0b7f96b9
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-09 15:30:25 +0530

    Delete parallaom.py

--- Module_3/random/parallaom.py
+++
@@ -1,6 +0,0 @@
-#Write a Python program to calculate the area of a parallelogram
-
-b = float(input("WEnter Base of parallelogram"))
-h = float(input("Enter height of Parallelogramn"))
-area = b*h
-print("Area of parallelogram",area)
