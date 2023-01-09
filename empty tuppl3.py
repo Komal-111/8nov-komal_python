db47dd4ba2513d60528af0975d39d3428c88a932
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-09 13:59:30 +0530

    Delete empty tuppl3.py

--- empty tuppl3.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to remove an empty tuple(s) from a list of tuples. 
-tuple =[(), (0,1,2,3),(6,7,8)]
-tuple = [t for t in tuple if t]
-print(tuple)
-
-'''L = [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
-L = [t for t in L if t]
-print(L)'''
