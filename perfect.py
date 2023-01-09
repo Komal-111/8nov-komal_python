fb5d0f71c8df17ab4b5c7c9454bb01a3a04b9e7a
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-09 13:57:06 +0530

    Delete perfect.py

--- perfect.py
+++
@@ -1,9 +0,0 @@
-#Write a Python function to check whether a number is perfect or not. 
-n=int(input("Enter number"))
-def perfect_num(n):
-    sum =0
-    if n>=sum:
-        print("the num is perfect")
-    else:
-        print("the num is not perfect...")
-perfect_num(n)
