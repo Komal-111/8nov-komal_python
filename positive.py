0b80a231f072e361243eae7fa26af795a0d54607
Komal-111 <118002852+Komal-111@users.noreply.github.com>
2023-01-09 13:58:31 +0530

    Delete positive.py

--- positive.py
+++
@@ -1,8 +0,0 @@
-#programmme positive negative or zero
-num=int(input("Enter a number:"))
-if num>=0:
-    print("This  number is positive")
-else :
-    print(" This  number is nagative")
-if num==0:
-    print("this number is zero")
